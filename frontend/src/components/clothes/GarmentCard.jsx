1d79f62dd9bc17c7536a11479dc3af87b91f7480
Carolina Oldertz <carolina@MacBook-Air.local>
2026-02-27 16:31:58 +0100

    continued with gramentcard and container and stared to look at implementing stores with zustand

--- frontend/src/components/clothes/GarmentCard.jsx
+++ frontend/src/components/clothes/GarmentCard.jsx
@@ -1,11 +1,25 @@
 import styled from "styled-components"
 
-export const GarmentCard = () => {
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const StyledImg = styled.img`
+  max-width: 50%;
+  border: 1px solid red;
+`
+
+const StyledInfoConatiner = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+export const GarmentCard = ({ garment }) => {
   return (
     <StyledContainer>
-      <StyledImg src={imageUrl} alt={`picture of garment type: "{garmentType}`}/>
+      <StyledImg src={garment.imageUrl} alt={`picture of garment type: "{garment.garmentType}`}/>
       <StyledInfoConatiner>
-        
         <p>{garmentType}</p>
       </StyledInfoConatiner>
     </StyledContainer>
